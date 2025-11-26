45811cccfcdbe0116789569a5f38f651ca779881
mac <mac@MacBook-Pro.local>
2025-11-26 04:05:41 +0200

    Final: Confirm CORS fix implemented

--- src/user/userValidation.js
+++ src/user/userValidation.js
@@ -5,12 +5,12 @@ import Joi from 'joi'
 const userSignUpSchema = Joi.object({
         name:Joi.string().min(3).max(30).required(),
         email:Joi.string().email().required(),
-        password:Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
+        password:Joi.string().min(8).max(50).required(),
 })
 
 const userSigninSchema = Joi.object({
         email:Joi.string().email().required(),
-        password:Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
+        password:Joi.string().min(8).max(50).required(),
 })
 
 
